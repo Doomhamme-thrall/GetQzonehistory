d0b08fac1316d36435442f77ef669612ee8ced05
Hu Zheyu <62643951+Doomhamme-thrall@users.noreply.github.com>
2024-11-07 10:47:50 +0000

    main

---
+++ test.py
@@ -0,0 +1,15 @@
+# import builtins;
+# for item in dir(builtins):
+#     print(item)
+# help(input)
+
+#定义函数
+
+# def step_calculate(num):
+#     result=0;
+#     for i in range(num+1):
+#         result+=i
+#     return result
+
+# print(step_calculate(100))
+
